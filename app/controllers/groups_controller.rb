2f557a8e1d2686ffa111762524513c124a8ef5e5
高尾智紀 <hawks0903.taka@cloud.com>
2018-09-11 16:38:18 +0900

    Edit search1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+ 
+ 	before_action :set_group
+
 	def index	
 	end
 
@@ -17,8 +20,6 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-		@group = Group.find(params[:id])
-		@group.users << current_user
 	end
 
 	def update
@@ -32,7 +33,7 @@ class GroupsController < ApplicationController
 	private
 
 	def group_params
-		params.require(:group).permit(:name, { :user_ids => [] })
+		params.require(:group).permit(:name, { user_ids: [] })
 	end
 
 	def set_group
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resource :user, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resource :user, only: [:edit, :update] do
+    collection do
+      get 'search'
+    end
+  end
+  resources :groups, only: [:new, :create, :edit, :update,] do
     resources :messages, only: [:index, :create]
   end
 end
